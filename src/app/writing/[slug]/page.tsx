1294ed143119173c8dd10d6403adbd89cbc1f820
Arın Deniz Köklü <arin.koklu@forto.com>
2026-01-12 18:45:29 +0300

    create reusable footer component

--- src/app/writing/[slug]/page.tsx
+++ src/app/writing/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+import Footer from '@/components/footer'
 import { useTranslation } from 'react-i18next'
 
 export default function Post() {
@@ -24,52 +24,7 @@ export default function Post() {
         </header>
       </article>
 
-      <section className="closingSection">
-        <div className="closingPanel">
-          <div className="closingNav">
-            <Link href="/writing">{t('nav.writings')}</Link>
-            <Link href="/meet-ece">{t('nav.meetEce')}</Link>
-            <Link href="/recommendations">{t('nav.recommendations')}</Link>
-            <Link href="/heroes">{t('nav.heroes')}</Link>
-          </div>
-          <div className="closingMeta">
-            <div className="closingLinks">
-              <Link href="/privacy">{t('footer.privacy')}</Link>
-              <Link href="/disclaimer">{t('footer.disclaimer')}</Link>
-              <Link href="/contact">{t('footer.contact')}</Link>
-            </div>
-            <div className="closingSocial">
-              <a
-                href="https://www.instagram.com/ececamci"
-                aria-label="Instagram"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="/ig.svg" alt="" />
-              </a>
-              <a
-                href="https://www.youtube.com/@msececamci"
-                aria-label="YouTube"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="/yt.svg" alt="" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/ececamci/"
-                aria-label="LinkedIn"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src="/in.svg" alt="" />
-              </a>
-            </div>
-            <div className="closingCopyright">
-              {t('footer.copyright', { year: 2025 })}
-            </div>
-          </div>
-        </div>
-      </section>
+      <Footer />
     </main>
   )
 }
