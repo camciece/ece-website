e39cddab212fb4d21973599a92b29c25ab7e1a04
Ece Çamcı <camciece@gmail.com>
2026-01-13 13:07:54 +0300

    hjkhl

--- src/lib/locale.ts
+++ src/lib/locale.ts
@@ -1,7 +1,7 @@
 export const locales = ['en', 'tr'] as const
 export type Locale = (typeof locales)[number]
 
-export const defaultLocale: Locale = 'en'
+export const defaultLocale: Locale = 'tr'
 
 export function isLocale(value?: string | null): value is Locale {
   return locales.includes(value as Locale)
